15be70520d68778d45450e46525c74c7df943b71
ivan <ivanvaluchevalex1996@gmail.com>
2023-02-28 01:29:35 +0300

    add

--- src/utils/truncate.js
+++ src/utils/truncate.js
@@ -1,5 +1,5 @@
 export default function truncate(str) {
-  const text = str.slice(0, 140);
+  const text = str.slice(0, 90);
   const a = text.split(" ");
   a.splice(a.length - 1, 1);
   const res = a.join(" ");
